25fa3e5d54542c5e2dd22a3c842b0e55fb5de876
Wingrog <ciborgue42@hotmail.fr>
2020-08-10 21:21:41 +0200

    modif nav

--- View/Parts/navbar.php
+++ View/Parts/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Starwars MVC</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link" href="index.php?controller=default&action=home">Home</a>
+                <a class="nav-link" href="index.php?controller=default&action=home">Accueil</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="index.php?controller=planet&action=listPlanet">Les planètes</a>
